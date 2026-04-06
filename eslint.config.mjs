29fb0fcba60d954953c9bd9fad9a975c3c612321
Mark <mark@example.com>
2026-04-06 15:54:25 -0500

    Build pizza website

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,24 @@
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
+import reactPlugin from "eslint-plugin-react";
+// import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+// import css from "@eslint/css";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default defineConfig([
+  js.configs.recommended,
+  reactPlugin.configs.flat["jsx-runtime"],
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+    ...reactPlugin.configs.flat.recommended,
+
+    plugins: {
+      js,
+      reactPlugin,
+      "react-hooks": reactHooks,
+    },
     extends: ["js/recommended"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
@@ -18,12 +28,24 @@ export default defineConfig([
         },
       },
     },
+    rules: {
+      // ...reactPlugin.configs.recommended.rules,
+      // ...reactPlugin.configs["jsx-runtime"].rules,
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
+      ...reactHooks.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
-  pluginReact.configs.flat.recommended,
-  {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
-  },
+  // pluginReact.configs.flat.recommended,
+  // {
+  //   files: ["**/*.css"],
+  //   plugins: { css },
+  //   language: "css/css",
+  //   extends: ["css/recommended"],
+  // },
 ]);
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Order from "./Order.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Pixel Perfect Pizzas</h1>
+      <Order />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+// root.render(React.createElement(App));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
