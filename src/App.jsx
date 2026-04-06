ad1d90c0d806c42805443255f9b1c8429034443b
Mark <mark@example.com>
2026-04-06 16:09:24 -0500

    Add Pizza of the Day

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order.jsx";
+import PizzaOfTheDay from "./PizzaOfTheDay.jsx";
 
 const App = () => {
   return (
     <div>
       <h1>Pixel Perfect Pizzas</h1>
       <Order />
+      <PizzaOfTheDay />
     </div>
   );
 };
