a27064d26dc993b4c06ca2d97225f4da81aedf52
Mark <mark@example.com>
2026-04-08 14:10:54 -0500

    Add tests and test coverage

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,11 @@ export default defineConfig({
   ],
   test: {
     environment: "happy-dom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*.{js,jsx,ts,tsx}"],
+    },
   },
   server: {
     proxy: {
